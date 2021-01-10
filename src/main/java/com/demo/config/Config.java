4d436db6ab2cb6802a96ad6975c0823427fe3297
Konstantin <kizin.kanstantsin@codex-soft.com>
2021-01-10 18:53:49 +0300

    cors fasdad

--- src/main/java/com/demo/config/Config.java
+++ src/main/java/com/demo/config/Config.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
@@ -33,7 +34,7 @@ public class Config implements WebMvcConfigurer {
     }
 
 
-    @Bean
+/*    @Bean
     @Autowired
     public FilterRegistrationBean corsFilter(CustomCorsFilter filter) {
         FilterRegistrationBean<CustomCorsFilter> registration = new FilterRegistrationBean<>();
@@ -43,6 +44,18 @@ public class Config implements WebMvcConfigurer {
         registration.setOrder(1);
         return registration;
 
+    }*/
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+            }
+        };
     }
 
 
