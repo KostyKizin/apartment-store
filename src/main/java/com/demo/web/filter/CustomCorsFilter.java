d5dc6e622ffa7baffeeb2e53b3c8417b9f50d406
Konstantin <kizin.kanstantsin@codex-soft.com>
2021-01-10 17:22:52 +0300

    back

--- src/main/java/com/demo/web/filter/CustomCorsFilter.java
+++ src/main/java/com/demo/web/filter/CustomCorsFilter.java
@@ -15,12 +15,12 @@ public class CustomCorsFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        filterChain.doFilter(servletRequest, servletResponse);
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 }
